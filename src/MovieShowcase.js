f39af31308b09b4d86d09b10a9d925faadbc4476
Yisrael Gurkow <yisrelg3@gmail.com>
2021-05-20 00:45:46 -0400

    commit tree c5473ddccf76f1f10a276e39d0843e3779e439e9

--- src/MovieShowcase.js
+++ src/MovieShowcase.js
@@ -6,6 +6,9 @@ export default class MovieShowcase extends Component {
 
   generateMovieCards = () => {
     // map over your movieData array and return an array of the correct JSX
+    return movieData.map(cardData => { return <MovieCard title={cardData.title} 
+      IMDBRating={cardData.IMDBRating} genres={cardData.genres} 
+      poster={cardData.poster}/>})
   }
 
   render() {
